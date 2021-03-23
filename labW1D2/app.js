7f463ae8a49f3b00b89c86706e1984fcad6849c2
Eyasu Gebrehiwot <jgebrehiwot@gmail.com>
2021-03-23 14:50:13 -0500

    The First Attempt

--- labW1D2/app.js
+++ labW1D2/app.js
@@ -1,4 +1,29 @@
-function load() {
-	let id = document.getElementById("result");
-	id.innerHTML = "Lab W1D2";
+/* eslint-disable require-jsdoc */
+/* eslint-disable no-unused-vars */
+"use strict";
+
+// See tests.js for behavior
+function nameString(obj) {
+    if (obj.length === 0) {
+        return "Object has no properties";
+    } else if (obj === undefined) {
+        return "No object Passed";
+    } else if (obj === null) {
+        return "Null argument passed";
+    } else if (obj.length != 0) {
+        let output = '';
+        for (let key in obj) {
+            if (obj[key] > 1) {
+                output += key
+            } else {
+                key
+            }
+            return output;
+        
+        }
+        
+    }
 }
+console.log(nameString({a:3, b:5,c:6}));
+
+
