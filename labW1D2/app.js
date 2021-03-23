68903578d7f82ad149b4b9d5c2fa77ccaf9fdf0d
Eyasu Gebrehiwot <jgebrehiwot@gmail.com>
2021-03-23 17:03:48 -0500

    All Testes Passed

--- labW1D2/app.js
+++ labW1D2/app.js
@@ -4,26 +4,35 @@
 
 // See tests.js for behavior
 function nameString(obj) {
-    if (obj.length === 0) {
-        return "Object has no properties";
-    } else if (obj === undefined) {
-        return "No object Passed";
-    } else if (obj === null) {
-        return "Null argument passed";
-    } else if (obj.length != 0) {
-        let output = '';
-        for (let key in obj) {
-            if (obj[key] > 1) {
-                output += key
-            } else {
-                key
+    
+        if (obj == 0) {
+            return "object has no properties";
+        } else if (obj === undefined) {
+            return "no object passed";
+        } else if (obj === null) {
+            return "null argument";
+        } else if (obj !== 0) {
+            let newArray = [];
+            for (let key in obj) {
+               
+                if (obj[key] > 1) {
+                    newArray.push(key);
+                } else
+                    key
             }
-            return output;
-        
-        }
-        
+            //console.log(newArray);
+            newArray.sort()
+            let newsortedArray=newArray.join(".");
+            //console.log(newsortedArray);
+            return newsortedArray;
     }
-}
-console.log(nameString({a:3, b:5,c:6}));
+    
+        }
+    
 
+console.log(nameString([]));
+console.log(nameString());
+console.log(nameString(null));
+console.log(nameString({ a: 3}));
+console.log(nameString({a:3, c:5,b:6}));
 
